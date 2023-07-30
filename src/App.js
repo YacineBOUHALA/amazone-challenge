5db314bc4c2708750376fb25e1a35cbf1f17b19d
YacineBOUHALA <docyacinou@gmail.com>
2023-07-30 22:42:15 +0200

    upadate app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
 import Header from './Header';
-import Home from './Home'
+import Home from './Home';
+import Checkout from './Checkout.js'
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Login  from './Login';
 
 function App() {
+  //const location = useLocation();
+  //const isLoginPage = location.pathname === '/login';
   return (
-    //BEM
-    <div className="app">
-      {/*header*/}
-      <Header/>
-       {/*body*/}
-       <Home/>
-    </div>
+    <BrowserRouter>
+      {/* BEM */}
+      <div className="app">
+      <Routes>
+       
+          
+        </Routes>
+          {/* header */}
+          {/*<Header />*}
+        
+        {/* body */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/checkout' element={<Checkout/>}/>
+          {/* Ajoutez d'autres routes ici si nécessaire */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
