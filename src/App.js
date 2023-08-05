b396d87b4e5942b8680e0dd3cbfeda4e183d725b
YacineBOUHALA <docyacinou@gmail.com>
2023-08-05 20:48:44 +0200

    add athentication logique

--- src/App.js
+++ src/App.js
@@ -4,8 +4,33 @@ import Home from './Home';
 import Checkout from './Checkout.js'
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Login  from './Login';
+import { useEffect } from 'react';
+import {auth} from './firebase'
+import {useStateValue} from './StateProvider'
 
 function App() {
+  const [{}, dispatch] = useStateValue()
+  useEffect(() =>{
+    //will only run ones when the app component loads
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('the user isd >>>', authUser)
+
+      if(authUser){
+        //hs is logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }else{
+        //user loggged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
   //const location = useLocation();
   //const isLoginPage = location.pathname === '/login';
   return (
