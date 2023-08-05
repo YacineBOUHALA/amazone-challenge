ae8d024ee1e7bce24f11260c52ecdc418f350224
YacineBOUHALA <docyacinou@gmail.com>
2023-08-05 20:50:31 +0200

    add firebase autentification

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDKwibQddTVzcKULS6TwA8eNoekeRkFIsY",
@@ -8,3 +13,11 @@ const firebaseConfig = {
   appId: "1:587277451325:web:17a1afa5e662639f7d6a54",
   measurementId: "G-QXZ685V49J"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+
+export {db, auth};
