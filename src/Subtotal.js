d441fc66158e21979e07375d32f709d19da5e85f
YacineBOUHALA <docyacinou@gmail.com>
2023-08-06 17:27:22 +0200

    initiate payment in subtotal file

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,25 +2,29 @@ import React from 'react'
 import "./Subtotal.css"
 import CurrencyFormat from "react-currency-format"
 import { useStateValue } from './StateProvider';
+import { useNavigate } from 'react-router-dom';
+
+
+export function basketTotal(basket) {
+  let total = 0;
+  for (let i = 0; i < basket.length; i++) {
+    total = total + basket[i].price;
+  }
+  return total.toFixed(2);
+}
+
 
 function Subtotal() {
+  const navigate = useNavigate()
   const [{basket}, dispatch] = useStateValue();
 
-  function basketTotal(){
-    let total = 0;
-    for(let i= 0; i < basket.length; i++){
-      total = total+ basket[i].price
-    }
-    console.log('baket toatal',total.toFixed(2))
-    return total.toFixed(2);
-  }
   return (
     <div className='subtotal'>
         <CurrencyFormat renderText={(value)=>(
             <>
                 <p>
                     Subtotal ({basket?.length}):
-                    <strong> {basketTotal()} $</strong>
+                    <strong> {basketTotal(basket)} $</strong>
                 </p>
                 <small className='subtotal__gift'>
                     <input type='checkbox'/> Cette Commande contien un cadeau
@@ -34,7 +38,7 @@ function Subtotal() {
         prefix={'f'}
         
         />
-      <button>Proceed to checkout</button>
+      <button onClick={e => navigate('/payment')}>Proceed to checkout</button>
     </div>
   )
 }
