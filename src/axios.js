603add2ea640467ea8c5dee878a6f802bc0d35fd
YacineBOUHALA <docyacinou@gmail.com>
2023-08-27 15:09:00 +0200

    add endpoint

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: '...' // cloud function url
+    baseURL: 'http://127.0.0.1:5001/clone-94b7e/us-central1/api' // cloud function url
 })
 
 export default instance
